53f5913d4fcdf9460adb4a242237dcda419c55d1
MRITUNJAY SRIVASTAVA <mritunjay.nptel@gmail.com>
2017-05-05 18:52:36 +0530

    Add files via upload

---
+++ Hostel Management/medical/insert medicine recevied.php
@@ -0,0 +1,38 @@
+
+																	<!------MEDICINE GIVEN INSERTION-------->
+
+<div style="height:100%; width:46%; font-family:'Courier New', Courier, monospace; font-size:large;">
+ <table cellspacing="29%">
+  <tr>
+	<td width="90">
+	  MEDICINE NAME    
+	 </td>
+	<td width="146"><input type="text" name="user" placeholder="Enter name of medicine name">
+	</td>
+  </tr>
+  <tr>
+   <td>
+	QUANTITY
+   </td>
+   <td>
+    <input type="text" name="pass" placeholder="Enter no. of medicine">
+   </td>
+  </tr>
+  <tr>
+   <td height="29">
+	DATE OF RECEVING   
+   </td>
+   <td>
+     <input type="text" name="pass" placeholder="Enter no. of medicine">   </td>
+  </tr>
+  
+  <tr>
+   <td>
+	 <center>      <input type="submit" value="SUBMIT" name="medgiven">        </center>
+   </td>
+   <td>
+	 <center>      <input type="reset" value="CLEAR">        </center>
+   </td>
+  </tr>  
+ </table>   
+</div>
